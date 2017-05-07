b3d8f18cdb2009af4896b8effb6dc1af19235bad
wmmurrah <william.murrah@gmail.com>
2017-05-07 18:01:37 -0500

    added anscombe plots to visualization chapter

---
+++ R/plot_anscombe.R
@@ -0,0 +1,9 @@
+plot_anscombe <- function(data, x, y){
+  # Plots Anscombe 1973 Quartet data.
+  source("R/stat_smooth_func.R")
+  ggplot(data, aes_string(x = x, y = y)) +
+    geom_point() + ylim(0, 15) + xlim(0, 20) +
+    geom_smooth(method = lm, se = FALSE, fullrange = TRUE) +
+    stat_smooth_func(geom="text",method="lm",hjust=0,parse=TRUE,
+                     xpos = 10, ypos = 2.5)
+}
